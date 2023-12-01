a31560c5c3f0403ac148451abaf0de1df3f22b49
domantion1 <152414929+domantion1@users.noreply.github.com>
2023-12-01 19:05:16 +0100

    rzeczy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.example.skaner_proba_druga"
+
     compileSdk = 34
 
     defaultConfig {
@@ -27,6 +29,23 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
+android {
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -35,4 +54,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.gms:play-services-vision:20.1.3")
+
 }
+dependencies {
+    implementation("com.vanniktech:android-image-cropper:4.5.0")
+}
+
+
